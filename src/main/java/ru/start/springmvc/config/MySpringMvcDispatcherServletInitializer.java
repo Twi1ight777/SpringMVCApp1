5bbfffb35138fe065b87b65c90fe62f0eb042267
Mr. T <vlad.chiglinzev@mail.ru>
2025-03-10 20:38:57 +0700

    Realized class system

--- src/main/java/ru/start/springmvc/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/ru/start/springmvc/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,4 +1,20 @@
 package ru.start.springmvc.config;
 
-public class MySpringMvcDispatcherServletInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
--- src/main/java/ru/start/springmvc/controllers/FirstController.java
+++ src/main/java/ru/start/springmvc/controllers/FirstController.java
@@ -1,4 +1,12 @@
 package ru.start.springmvc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FirstController {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "hello_world";
+    }
 }
