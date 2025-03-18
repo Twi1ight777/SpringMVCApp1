29b84a4cc6e506b46595d4024b78a695e7a73b28
Mr. T <vlad.chiglinzev@mail.ru>
2025-03-18 16:25:09 +0700

    Realized features book/person views. Tested the project

--- src/main/java/ru/start/springmvc/controllers/BatchController.java
+++ src/main/java/ru/start/springmvc/controllers/BatchController.java
@@ -6,27 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.start.springmvc.dao.PersonDAO;
 
-@Controller
-@RequestMapping("/test-batch-update")
-public class BatchController {
-    private final PersonDAO personDAO;
-
-    @Autowired
-    public BatchController(PersonDAO personDAO) {
-        this.personDAO = personDAO;
-    }
-    @GetMapping
-    public String batchIndex() {
-        return "batch/index";
-    }
-    @GetMapping("/without")
-    public String withoutBatch() {
-        personDAO.testMultipleUpdate();
-        return "redirect:/people";
-    }
-    @GetMapping("/with")
-    public String withBatch() {
-        personDAO.testBatchUpdate();
-        return "redirect:/people";
-    }
-}
+//@Controller
+//@RequestMapping("/test-batch-update")
+//public class BatchController {
+//    private final PersonDAO personDAO;
+//
+//    @Autowired
+//    public BatchController(PersonDAO personDAO) {
+//        this.personDAO = personDAO;
+//    }
+//    @GetMapping
+//    public String batchIndex() {
+//        return "batch/index";
+//    }
+//    @GetMapping("/without")
+//    public String withoutBatch() {
+//        personDAO.testMultipleUpdate();
+//        return "redirect:/people";
+//    }
+//    @GetMapping("/with")
+//    public String withBatch() {
+//        personDAO.testBatchUpdate();
+//        return "redirect:/people";
+//    }
+//}
